e08cf815e190fb7d122cff30c2d581e107b81411
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2019-01-15 09:49:48 +0100

    Commit

---
+++ holamundo/HolaMundo.java
@@ -0,0 +1,23 @@
+/*Autor: igonzaleziglesias
+ *Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+ *Everyone is permitted to copy and distribute verbatim copies
+ *of this license document, but changing it is not allowed.
+ */
+package holamundo;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author igonzaleziglesias
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+    
+}
